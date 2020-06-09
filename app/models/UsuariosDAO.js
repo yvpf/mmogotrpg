5e6ce7027620ca3ec51359453b87ed3da4f32b6a
Yago Fernandes <yagovictorpessoafernandes2@gmail.com>
2020-06-09 10:37:27 -0300

    Atualização de criptografia para senhas de users

--- app/models/UsuariosDAO.js
+++ app/models/UsuariosDAO.js
@@ -1,3 +1,7 @@
+var crypto = require('crypto');
+
+
+
 function UsuariosDAO(connection){
     this._connection = connection();
 }
@@ -5,6 +9,8 @@ function UsuariosDAO(connection){
 UsuariosDAO.prototype.inserirUsuario = function(usuario){
     this._connection.open(function(err, mongoclient){
         mongoclient.collection("usuarios", function(err, collection){
+            var senha_criptografada = crypto.createHash("md5").update(usuario.senha).digest("hex");
+            usuario.senha = senha_criptografada;
             collection.insert(usuario);
             mongoclient.close();
         });
@@ -14,6 +20,10 @@ UsuariosDAO.prototype.inserirUsuario = function(usuario){
 UsuariosDAO.prototype.autenticar = function(usuario, req, res){
     this._connection.open(function (err, mongoclient) {
         mongoclient.collection("usuarios", function (err, collection) {
+
+            var senha_criptografada = crypto.createHash("md5").update(usuario.senha).digest("hex");
+            usuario.senha = senha_criptografada;
+
             collection.find(usuario).toArray(function(err, result){
                 if(result[0] != undefined){
                     req.session.autorizado = true;
